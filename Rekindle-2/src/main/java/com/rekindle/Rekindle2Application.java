d6270e94031167078a0d7c0b71b3ac28d4166a54
Aleix Falgueras Casals <aleix.falgueras@est.fib.upc.edu>
2018-04-03 10:51:48 +0200

    aleixfirst

---
+++ Rekindle-2/src/main/java/com/rekindle/Rekindle2Application.java
@@ -0,0 +1,12 @@
+package com.rekindle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rekindle2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rekindle2Application.class, args);
+	}
+}
