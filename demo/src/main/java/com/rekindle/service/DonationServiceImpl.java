eb3a1c0f67886a78464f1a98a645da48a5d82759
aalex <aalex@LAPTOP-398JMTQA>
2018-04-04 09:47:28 +0200

    Listar todas las donaciones

--- demo/src/main/java/com/rekindle/service/DonationServiceImpl.java
+++ demo/src/main/java/com/rekindle/service/DonationServiceImpl.java
@@ -2,6 +2,9 @@ package com.rekindle.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.rekindle.entities.Donation;
 import com.rekindle.repositories.DonationRepository;
 
