f3ed615629142c4cff18e8c516bf16d39f7f1333
roger.poch <roger.poch@est.fib.upc.edu>
2018-04-06 21:25:14 +0200

    destrussion

--- demo/src/main/java/com/rekindle/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rekindle.repositories;
-import javax.transaction.Transactional;
-
-import org.springframework.data.repository.CrudRepository;
-import com.rekindle.entities.User;
-
-@Transactional
-public interface UserRepository extends CrudRepository<User, String> {
-}
