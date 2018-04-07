1f2d790dd800a4396b65692c3d8c90ca41207bb6
roger.poch <roger.poch@est.fib.upc.edu>
2018-04-07 14:44:33 +0200

    Comandes SQL actualitzades

--- rekindle/database-init/creation.sql
+++ rekindle/database-init/creation.sql
@@ -1,4 +1,5 @@
-use local_rekindle;
+USE PES;
+USE local_rekindle;
 CREATE TABLE Refugee (
 	mail varchar(30) PRIMARY KEY,
 	password varchar(15) NOT NULL,
@@ -22,3 +23,43 @@ CREATE TABLE Volunteer (
 	surname1 varchar(20) NOT NULL,
 	surname2 varchar(20)
 );
+
+DELIMITER $$
+CREATE PROCEDURE `is_not_a_refugee` (IN mail VARCHAR(30))
+BEGIN
+ DECLARE count INTEGER;
+ SELECT count(*) INTO count
+ FROM Refugee r
+ WHERE r.mail = mail;
+    IF count > 0 
+  THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This user already exists as refugee';
+ END IF;
+END 
+$$
+
+DELIMITER $$
+CREATE TRIGGER user_exists_as_refugee BEFORE INSERT ON Volunteer 
+FOR EACH ROW 
+BEGIN CALL is_not_a_refugee(NEW.mail);
+END;
+$$
+
+DELIMITER $$
+CREATE PROCEDURE `is_not_a_volunteer` (IN mail VARCHAR(30))
+BEGIN
+ DECLARE count INTEGER;
+ SELECT count(*) INTO count
+ FROM Volunteer v
+ WHERE v.mail = mail;
+    IF count > 0 
+  THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This user already exists as volunteer';
+ END IF;
+END 
+$$
+
+DELIMITER $$
+CREATE TRIGGER user_exists_as_volunteer BEFORE INSERT ON Refugee 
+FOR EACH ROW 
+BEGIN CALL is_not_a_volunteer(NEW.mail);
+END;
+$$
