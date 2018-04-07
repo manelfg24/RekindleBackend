45ab215cbfe3d780980e1b011cd2ced5fe6952eb
alexsanchezz <alex.sanchez.gil@est.fib.upc.edu>
2018-04-07 11:53:07 +0200

    tablas

--- rekindle/database-init/creation.sql
+++ rekindle/database-init/creation.sql
@@ -1,17 +1,24 @@
-CREATE TABLE User (
+use local_rekindle;
+CREATE TABLE Refugee (
 	mail varchar(30) PRIMARY KEY,
-	userType char NOT NULL,
-    password varchar(15) NOT NULL,
+	password varchar(15) NOT NULL,
 	name varchar(20) NOT NULL,
-    surname1 varchar(20) NOT NULL,
-    surname2 varchar(20),	
-    
+	surname1 varchar(20) NOT NULL,
+	surname2 varchar(20), 
 	phoneNumber int,
-    birthdate date,
-    sex varchar(20),
-    country varchar(20),
-    town varchar(40),
-    ethnic varchar(20),
+	birthdate date,
+	sex varchar(20),
+	country varchar(20),
+	town varchar(40),
+	ethnic varchar(20),
 	bloodType varchar(20),
-    eyeColor varchar(20)
+	eyeColor varchar(20)
+);
+
+CREATE TABLE Volunteer (
+	mail varchar(30) PRIMARY KEY,
+	password varchar(15) NOT NULL,
+	name varchar(20) NOT NULL,
+	surname1 varchar(20) NOT NULL,
+	surname2 varchar(20)
 );
