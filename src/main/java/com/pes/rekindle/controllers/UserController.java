7a2ce244724e0f70b372858ebcf0d907d7b7cef9
Roger Poch <roger.poch@est.fib.upc.edu>
2018-05-04 17:28:24 +0200

    Fix

--- src/main/java/com/pes/rekindle/controllers/UserController.java
+++ src/main/java/com/pes/rekindle/controllers/UserController.java
@@ -115,8 +115,10 @@ public class UserController {
     public ResponseEntity<Object> logIn(@RequestBody LogInInfo logInInfo) {
     	System.out.println("Informacion del usuario: " + logInInfo.getMail() + " " + logInInfo.getPassword() + " " + logInInfo.getNewPassword());
     	Pair<Integer, Object> user = userService.exists(logInInfo.getMail(), logInInfo.getPassword());
-        if (user.getFirst()==0 || user.getFirst()==1)
-            return ResponseEntity.status(HttpStatus.OK).header("Tipo", user.getFirst().toString()).body(user.getSecond());
+        if (user.getFirst()==0 || user.getFirst()==1) {
+            //return ResponseEntity.status(HttpStatus.OK).header("Tipo", user.getFirst().toString()).body(user.getSecond());
+        	return ResponseEntity.status(HttpStatus.OK).body(user.getSecond());
+        }
         else 
         	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
