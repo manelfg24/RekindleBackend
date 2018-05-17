2c5fe28046329e32fdf0d8bf84837aa840b9f65a
alexsanchezz <alex.sanchez.gil@est.fib.upc.edu>
2018-05-17 20:54:51 +0200

    Liquibase rules

--- src/main/java/com/pes/rekindle/repositories/ChatRepository.java
+++ src/main/java/com/pes/rekindle/repositories/ChatRepository.java
@@ -1,28 +1,25 @@
 
 package com.pes.rekindle.repositories;
 
-import java.util.Collection;
-import java.util.Optional;
 import java.util.Set;
 
 import javax.transaction.Transactional;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
 
 import com.pes.rekindle.entities.Chat;
-import com.pes.rekindle.entities.Volunteer;
 
 @Transactional
 public interface ChatRepository extends Repository<Chat, String> {
-	public boolean existsByMailUser1(String mail);
-	public boolean existsByMailUser2(String mail);
-	
-	public Set<Chat> findByMailUser1(String mail);
-	public Set<Chat> findByMailUser2(String mail);
-	
-	public Chat findByMailUser1AndMailUser2(String mailUser1, String mailUser2);
-	public void save(Chat chat);
+    public boolean existsByMailUser1(String mail);
+
+    public boolean existsByMailUser2(String mail);
+
+    public Set<Chat> findByMailUser1(String mail);
+
+    public Set<Chat> findByMailUser2(String mail);
+
+    public void save(Chat chat);
+
+    public Chat findByMailUser1AndMailUser2(String mail, String mail2);
 }
