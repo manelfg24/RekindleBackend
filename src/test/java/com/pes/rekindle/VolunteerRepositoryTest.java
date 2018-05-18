b1404b9d1a83dffc2a523791fca957db5b7a50ee
alexsanchezz <alex.sanchez.gil@est.fib.upc.edu>
2018-05-18 09:04:53 +0200

    Liquibase running

--- src/test/java/com/pes/rekindle/VolunteerRepositoryTest.java
+++ src/test/java/com/pes/rekindle/VolunteerRepositoryTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
@@ -18,9 +17,8 @@ import com.pes.rekindle.repositories.VolunteerRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureTestDatabase
 @DataJpaTest
-@Sql()
+@Sql(scripts = "classpath:database-init/inserts.sql")
 public class VolunteerRepositoryTest {
 
     @Autowired
