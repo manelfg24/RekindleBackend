7da5966a5028a5bb65f1c62614397a8fe36dd05a
Roger Poch <roger.poch@est.fib.upc.edu>
2018-06-14 18:41:51 +0200

    Hashcodes y equals

--- src/main/java/com/pes/rekindle/entities/EducationEnrollmentKey.java
+++ src/main/java/com/pes/rekindle/entities/EducationEnrollmentKey.java
@@ -20,4 +20,33 @@ public class EducationEnrollmentKey implements Serializable{
 	public void setEducationId(Long educationId) {
 		this.educationId = educationId;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((educationId == null) ? 0 : educationId.hashCode());
+		result = prime * result + ((refugeeMail == null) ? 0 : refugeeMail.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EducationEnrollmentKey other = (EducationEnrollmentKey) obj;
+		if (educationId == null) {
+			if (other.educationId != null)
+				return false;
+		} else if (!educationId.equals(other.educationId))
+			return false;
+		if (refugeeMail == null) {
+			if (other.refugeeMail != null)
+				return false;
+		} else if (!refugeeMail.equals(other.refugeeMail))
+			return false;
+		return true;
+	}
 }
--- src/main/java/com/pes/rekindle/entities/LodgeEnrollmentKey.java
+++ src/main/java/com/pes/rekindle/entities/LodgeEnrollmentKey.java
@@ -20,5 +20,35 @@ public class LodgeEnrollmentKey implements Serializable{
 	public void setLodgeId(Long lodgeId) {
 		this.lodgeId = lodgeId;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((lodgeId == null) ? 0 : lodgeId.hashCode());
+		result = prime * result + ((refugeeMail == null) ? 0 : refugeeMail.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LodgeEnrollmentKey other = (LodgeEnrollmentKey) obj;
+		if (lodgeId == null) {
+			if (other.lodgeId != null)
+				return false;
+		} else if (!lodgeId.equals(other.lodgeId))
+			return false;
+		if (refugeeMail == null) {
+			if (other.refugeeMail != null)
+				return false;
+		} else if (!refugeeMail.equals(other.refugeeMail))
+			return false;
+		return true;
+	}
    
 }
