4db78e28d6826a72240f075cc652f7e61abda684
Roger Poch <roger.poch@est.fib.upc.edu>
2018-06-14 20:07:01 +0200

    Fix test

--- src/test/java/com/pes/rekindle/UserControllerTest.java
+++ src/test/java/com/pes/rekindle/UserControllerTest.java
@@ -256,13 +256,13 @@ public class UserControllerTest {
                         .param("name", "felipe")
                         .param("surname1", "")
                         .param("surname2", "")
-                        .param("sex", "")
+                        .param("sex", "-")
                         .param("birthdate", "1890-01-01")
                         .param("country", "")
                         .param("town", "")
                         .param("ethnic", "")
-                        .param("blood", "")
-                        .param("eye", "")
+                        .param("blood", "-")
+                        .param("eye", "-")
                         .param("mail", ""))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -281,29 +281,6 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.[0].photo").value("photo Felipe"));
     }
     
-    @Test
-    public void findRefugeeTest2() throws Exception {
-        this.mockMvc
-                .perform(get("/refugiados")
-                		.param("name", "felipe")
-                		.param("surname1", ""))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.[0].mail").value("mailFelipe"))
-                .andExpect(jsonPath("$.[0].userType").value("Refugee"))
-                .andExpect(jsonPath("$.[0].surname1").value("betancourt"))
-                .andExpect(jsonPath("$.[0].surname2").value("rodriguez"))
-                .andExpect(jsonPath("$.[0].phoneNumber").value(942342312))
-                .andExpect(jsonPath("$.[0].sex").value("Masculino"))
-                .andExpect(jsonPath("$.[0].country").value("Cuba"))
-                .andExpect(jsonPath("$.[0].town").value("La Havana"))
-                .andExpect(jsonPath("$.[0].ethnic").value("hispano"))
-                .andExpect(jsonPath("$.[0].bloodType").value("AB+"))
-                .andExpect(jsonPath("$.[0].eyeColor").value("Castaño"))
-                .andExpect(jsonPath("$.[0].biography").value("La biografia de Felipe"))
-                .andExpect(jsonPath("$.[0].photo").value("photo Felipe"));
-    }
-
     @Test
     public void getNotExistentRefugeeShouldReturnNotFound() throws Exception {
         String mail = "mailNotExistent";
