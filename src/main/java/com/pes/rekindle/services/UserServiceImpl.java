4ed41a8b285da734007e032141affcde4bf5e8c4
Felipe <felipeabr96@gmail.com>
2018-06-15 18:15:01 +0200

    Recuperación UserServiceImpl

--- src/main/java/com/pes/rekindle/services/UserServiceImpl.java
+++ src/main/java/com/pes/rekindle/services/UserServiceImpl.java
@@ -975,25 +975,88 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Set<DTOUser> getAllUsers() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Set<DTOUser> getAllUsers() {
+		Set<Refugee> refugees = refugeeRepository.findAll();
+		Set<Volunteer> volunteers = volunteerRepository.findAll();
+		Set<DTOUser> dtoUsers = new HashSet<DTOUser>();
+		
+		for (Refugee refugee : refugees) {
+			dtoUsers.add(new DTOUser(refugee));
+		}
+		
+		for (Volunteer volunteer : volunteers) {
+			dtoUsers.add(new DTOUser(volunteer));
+		}
+		
+		return dtoUsers;
+	}
 
     @Override
-    public Integer isUserEnabled(String mail) throws UserNotExistsException {
+	public Integer isUserEnabled(String mail) throws UserNotExistsException {
+		DTOUser dtoUser = getDTOUser(mail);
+		return dtoUser.getEnabled();
+	}
+    
+    private DTOUser getDTOUser(String mail) throws UserNotExistsException {
+		Optional<Volunteer> oVolunteer = volunteerRepository.findOptionalByMail(mail);
+		if (oVolunteer.isPresent()) {
+			return new DTOUser(oVolunteer.get());
+		}
+		else {
+			Optional<Refugee> oRefugee = refugeeRepository.findOptionalByMail(mail);
+			if (oRefugee.isPresent()) {
+				return new DTOUser(oRefugee.get());
+			}
+			else {
+				throw new UserNotExistsException();
+			}
+		}
+	}
 
-    }
+	@Override
+	public void modifyBannedStatus(String mail, int userFinalState) throws UserNotExistsException, UserStateAlreadyUpdatedException {
+		changeBanStatus(mail, userFinalState);								
+	}
+	
+	private void changeBanStatus(String mail, int userFinalState) throws UserNotExistsException, UserStateAlreadyUpdatedException {
+		Optional<Volunteer> oVolunteer = volunteerRepository.findOptionalByMail(mail);
+		if (oVolunteer.isPresent()) {
+			Volunteer volunteer = oVolunteer.get();
+			if (userFinalState == volunteer.getEnabled()) { //El estado del usuario es el mismo que el que nos pasan
+				throw new UserStateAlreadyUpdatedException();
+			}
+			else {
+				volunteer.setEnabled(userFinalState);
+				volunteerRepository.save(volunteer);
+			}
+		}
+		else {
+			Optional<Refugee> oRefugee = refugeeRepository.findOptionalByMail(mail);
+			if (oRefugee.isPresent()) {
+				Refugee refugee = oRefugee.get();
+				if (userFinalState == refugee.getEnabled()) {
+					throw new UserStateAlreadyUpdatedException();
+				}
+				else {
+					refugee.setEnabled(userFinalState);
+					refugeeRepository.save(refugee);
+				}
+			}
+			else {
+				throw new UserNotExistsException();
+			}
+		}
+	}
 
-    @Override
-    public void modifyBannedStatus(String mail, int userDesiredState)
-            throws UserNotExistsException, UserStateAlreadyUpdatedException {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void deleteReport(Long id) throws ReportNotExistsException {
-        reportRepository.deleteById(id);
-    }
+	@Override
+	public void deleteReport(Long id) throws ReportNotExistsException {
+		Optional<Report> oReport = reportRepository.findOptionalById(id);
+		if (oReport.isPresent()) {
+			//mirar apikey
+			reportRepository.deleteById(id);
+		}
+		else {
+			throw new ReportNotExistsException();
+		}
+	}
 }
